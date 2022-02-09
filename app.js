eb824db079b6efdaeb50db926a63c1ab4acaea4b
Michaël Dierick <michael.dierick@redpencil.io>
2022-02-09 14:41:14 +0100

    linting

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import insertUuid from './queries/insert-uuid';
 
 const CONFIG = readConfig();
 
-function readConfig() {
+function readConfig () {
   const configText = fs.readFileSync('/config/config.json', { encoding: 'utf8' });
   const config = JSON.parse(configText);
 
@@ -15,9 +15,9 @@ function readConfig() {
 }
 
 app.post('/run', async (_req, res) => {
-  let uris = new Set();
+  const uris = new Set();
 
-  for (let [type, graphs] of Object.entries(CONFIG)) {
+  for (const [type, graphs] of Object.entries(CONFIG)) {
     const subjectsWithout = await selectSubjectsWithoutUuid(type, graphs);
     if (subjectsWithout.length > 0) {
       console.log(`Found ${subjectsWithout.length} objects of type <${type}> without a uuid. Handling now.`);
@@ -28,24 +28,24 @@ app.post('/run', async (_req, res) => {
     }
   }
 
-  let ret = [];
+  const ret = [];
 
   uris.forEach((uri) => {
     ret.push({
-      "uri": uri,
-    })
-  })
+      uri: uri
+    });
+  });
 
   return res.status(200).send(JSON.stringify(
     {
-      "data": ret
+      data: ret
     }
   ));
 });
 
 app.post('/delta', bodyParser.json(), async (req, res) => {
   res.status(202).send();
-  for (let [type, graphs] of Object.entries(CONFIG)) {
+  for (const [type, graphs] of Object.entries(CONFIG)) {
     const insertedSubjects = filterDeltaForInsertedType(req.body, type);
     if (insertedSubjects.length > 0) {
       console.log(`Received ${insertedSubjects.length} object inserts of type ${type} through deltas. Handling now.`);
